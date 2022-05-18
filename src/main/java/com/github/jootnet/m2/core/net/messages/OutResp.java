c001446393289e3491432a3ec3c62d4b7d38bc24
jootnet <shawryan@outlook.com>
2022-05-18 17:17:22 +0800

    normal

--- src/main/java/com/github/jootnet/m2/core/net/messages/OutResp.java
+++ src/main/java/com/github/jootnet/m2/core/net/messages/OutResp.java
@@ -18,7 +18,8 @@ public class OutResp extends Message {
 		deSerializers.put(MessageType.OUT_RESP, buffer -> {
 			var code = buffer.getInt();
 			var serverTip = unpackString(buffer);
-			return new OutResp(code, serverTip);
+			var chrName = unpackString(buffer);
+			return new OutResp(code, serverTip, chrName);
 		});
 	}
 	
@@ -32,10 +33,15 @@ public class OutResp extends Message {
 	 * 服务端提示信息
 	 */
 	public String serverTip;
+	/**
+	 * 角色昵称
+	 */
+	public String chrName;
 
-	public OutResp(int code, String serverTip) {
+	public OutResp(int code, String serverTip, String chrName) {
 		this.code = code;
 		this.serverTip = serverTip;
+		this.chrName = chrName;
 	}
 
 	@Override
@@ -47,6 +53,7 @@ public class OutResp extends Message {
 	protected void packCore(DataOutput buffer) throws IOException {
 		buffer.writeInt(code);
 		packString(serverTip, buffer);
+		packString(chrName, buffer);
 	}
 
 }
