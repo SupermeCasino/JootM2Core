de5bf31c7f4182a6ffd98d1291b6d2df0c394386
jootnet <shawryan@outlook.com>
2022-05-21 16:02:41 +0800

    modifypsw & deletechr

--- src/main/java/com/github/jootnet/m2/core/net/MessageType.java
+++ src/main/java/com/github/jootnet/m2/core/net/MessageType.java
@@ -8,19 +8,11 @@ public enum MessageType {
      * 更新人物动作
      */
     HUM_ACTION_CHANGE(1001),
-    /**
-     * 登陆
-     * <br>
-     * 客户端发送用户名密码到服务端
-     */
+    /** 登陆 */
 	LOGIN_REQ(1002),
-	/**
-	 * 登陆结果
-	 */
 	LOGIN_RESP(1003),
 	/** 进入游戏世界 */
 	ENTER_REQ(1004),
-	/** 进入游戏世界结果 */
 	ENTER_RESP(1005),
 	/** 客户端连上时服务器返回一些配置信息 */
 	SYS_INFO(1006),
@@ -32,14 +24,18 @@ public enum MessageType {
 	NEW_CHR_RESP(1010),
 	/** 离开游戏世界 */
 	OUT_REQ(1011),
-	/** 离开游戏世界结果 */
 	OUT_RESP(1012),
 	/** 被踢出登录 */
 	KICKED_OUT(1013),
 	/** 退出登录 */
 	LOGOUT_REQ(1014),
-	/** 退出登录结果 */
 	LOGOUT_RESP(1015),
+	/** 修改密码 */
+	MODIFY_PSW_REQ(1016),
+	MODIFY_PSW_RESP(1017),
+	/** 删除角色 */
+	DELETE_CHR_REQ(1018),
+	DELETE_CHR_RESP(1019),
 	;
 
     private int id_;
